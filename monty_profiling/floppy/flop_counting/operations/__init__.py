e6116032180b0c324f020db40b3aa8582fe8208e
hlee9212 <hlee@numenta.com>
2025-01-14 11:35:29 -0800

    Update init file
    Addresses:
    https://github.com/thousandbrainsproject/monty_lab/pull/9#discussion_r1914727689

--- monty_profiling/floppy/flop_counting/operations/__init__.py
+++ monty_profiling/floppy/flop_counting/operations/__init__.py
@@ -3,19 +3,3 @@ from .matmul import *
 from .numpy_functions import *
 from .trigonometry import *
 from .linalg import *
-
-from . import (
-    arithmetic,
-    matmul,
-    numpy_functions,
-    trigonometry,
-    linalg,
-)
-
-__all__ = (
-    *arithmetic.__all__,
-    *matmul.__all__,
-    *numpy_functions.__all__,
-    *trigonometry.__all__,
-    *linalg.__all__,
-)
