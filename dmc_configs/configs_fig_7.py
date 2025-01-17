96b3d49e9378079f79a3350c1af338cff69d6aeb
hlee9212 <hlee@numenta.com>
2025-01-17 12:11:45 -0800

    Move config file

---
+++ dmc_configs/configs_fig_7.py
@@ -0,0 +1,15 @@
+"""
+Implements `dist_agent_1lm_randrot_nohyp_x_percent_20p`
+"""
+
+import copy
+from dmc_configs.dmc_eval_experiments import dist_agent_1lm_nohyp_randrot
+
+dist_agent_1lm_nohyp_randrot_x_percent_20p = copy.deepcopy(dist_agent_1lm_nohyp_randrot)
+
+################################################################################
+# dist_agent_1lm_randrot_nohyp_x_percent_5p (Figure 7)                         #
+################################################################################
+
+dist_agent_1lm_randrot_nohyp_x_percent_5p = copy.deepcopy(dist_agent_1lm_nohyp_randrot)
+print(dist_agent_1lm_randrot_nohyp_x_percent_5p)
