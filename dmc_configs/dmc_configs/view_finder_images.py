5c4ed21213f8f9486366eeb4cf12865c6c589acf
Scott Knudstrup <scottknudstrup@gmail.com>
2025-01-20 14:07:55 -0800

    Initial commit

---
+++ dmc_configs/dmc_configs/view_finder_images.py
@@ -0,0 +1,350 @@
+"""
+This module contains configs, a logger, and a motor policy for generating RGBD images
+of objects taken from the viewfinder. The motor policy ensures that the whole
+object fits within the view-finder's frame. It does this by moving forward until the
+object enters a small buffer region around the viewfinder's frame. The logger saves the
+images as .npy files and writes a jsonl file containing metadata about the object
+and pose for each image.
+
+This module currently requires a pretrained model.
+
+The primary use case for this module is to generate object images used for training
+and testing traditional models (e.g., vision transformers).
+
+"""
+
+import copy
+import json
+import logging
+import os
+import shutil
+from pathlib import Path
+from typing import Mapping
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tbp.monty.frameworks.actions.action_samplers import ConstantSampler
+from tbp.monty.frameworks.actions.actions import (
+    MoveForward,
+)
+from tbp.monty.frameworks.config_utils.config_args import (
+    EvalLoggingConfig,
+    MontyArgs,
+    MotorSystemConfigInformedNoTransStepS20,
+    PatchAndViewMontyConfig,
+    get_cube_face_and_corner_views_rotations,
+)
+from tbp.monty.frameworks.config_utils.make_dataset_configs import (
+    EnvironmentDataloaderPerObjectArgs,
+    EvalExperimentArgs,
+    PatchViewFinderMountHabitatDatasetArgs,
+    PredefinedObjectInitializer,
+    RandomRotationObjectInitializer,
+)
+from tbp.monty.frameworks.config_utils.policy_setup_utils import (
+    make_informed_policy_config,
+)
+from tbp.monty.frameworks.environments import embodied_data as ED
+from tbp.monty.frameworks.environments.ycb import DISTINCT_OBJECTS, SHUFFLED_YCB_OBJECTS
+from tbp.monty.frameworks.experiments import MontyObjectRecognitionExperiment
+from tbp.monty.frameworks.loggers.monty_handlers import MontyHandler
+from tbp.monty.frameworks.models.buffer import BufferEncoder
+from tbp.monty.frameworks.models.motor_policies import (
+    InformedPolicy,
+    get_perc_on_obj_semantic,
+)
+
+"""
+Basic setup
+-----------
+"""
+# Specify parent directory where all experiment should live under.
+output_dir = os.path.expanduser("~/tbp/results/monty/projects/view_finder_images")
+
+# Where to find the pretrained model. Not really used, but necessary
+# to set up an eval experiment.
+model_path = os.path.expanduser(
+    "~/tbp/results/dmc/pretrained_models/dist_agent_1lm/pretrained/model.pt"
+)
+
+object_names = SHUFFLED_YCB_OBJECTS
+train_rotations = get_cube_face_and_corner_views_rotations()
+random_rotations_5 = [
+    [19, 339, 301],
+    [196, 326, 225],
+    [68, 100, 252],
+    [256, 284, 218],
+    [259, 193, 172],
+]
+
+
+class ViewFinderRGBDHandler(MontyHandler):
+    """Save RGBD from view finder and episode metadata at the end of each episode."""
+
+    def __init__(self):
+        self.report_count = 0
+        self.save_dir = None
+        self.view_finder_id = None
+
+    @classmethod
+    def log_level(cls):
+        return "DETAILED"
+
+    def initialize(self, data, output_dir, episode, mode, **kwargs):
+        # Create output directory, putting existing directory into another
+        # location if it exists.
+        output_dir = Path(output_dir).expanduser()
+        self.save_dir = output_dir / "view_finder_rgbd"
+        if self.save_dir.exists():
+            old_dir = output_dir / "view_finder_rgbd_old"
+            if old_dir.exists():
+                shutil.rmtree(old_dir)
+            self.save_dir.rename(old_dir)
+        self.save_dir.mkdir(parents=True)
+
+        # Create arrays subdirectory.
+        arrays_dir = self.save_dir / "arrays"
+        arrays_dir.mkdir()
+
+        # Determine which sensor module ID to use. Probably always 1.
+        sm_ids = [k for k in data["DETAILED"][episode].keys() if k.startswith("SM_")]
+        sm_nums = [int(name.split("_")[-1]) for name in sm_ids]
+        self.view_finder_id = f"SM_{max(sm_nums)}"
+
+    def report_episode(self, data, output_dir, episode, mode="train", **kwargs):
+        """
+        Changed name to report episode since we are currently running with
+        reporting and flushing exactly once per episode
+        """
+
+        if self.report_count == 0:
+            self.initialize(data, output_dir, episode, mode, **kwargs)
+
+        output_data = dict()
+        output_data["episode"] = episode
+        if mode == "eval":
+            target_info = data["BASIC"]["eval_targets"][episode]
+            output_data["object"] = target_info["primary_target_object"]
+            output_data["rotation"] = target_info["primary_target_rotation_euler"]
+
+        # Combine RGB and depth into a single RGBD image.
+        obs = data["DETAILED"][episode][self.view_finder_id]["raw_observations"][-1]
+        rgba = obs["rgba"]
+        depth = obs["depth"]
+
+        rgbd = rgba / 255.0
+        rgbd[:, :, 3] = depth
+
+        # Save the image.
+        arrays_dir = self.save_dir / "arrays"
+        array_path = arrays_dir / f"{episode}.npy"
+        np.save(array_path, rgbd)
+
+        # Save the metadata.
+        metadata_path = self.save_dir / "episodes.jsonl"
+        with open(metadata_path, "a") as f:
+            json.dump(output_data, f, cls=BufferEncoder)
+            f.write(os.linesep)
+
+        self.report_count += 1
+
+    def close(self):
+        pass
+
+
+class FramedObjectPolicy(InformedPolicy):
+    """Custom motor policy that helps keep the object in-frame"""
+
+    def move_close_enough(
+        self,
+        raw_observation: Mapping,
+        view_sensor_id: str,
+        target_semantic_id: int,
+        multiple_objects_present: bool,
+    ):
+        # ) -> Tuple[Union[Action, None], bool]:
+        """At beginning of episode move close enough to the object.
+
+        Used the raw observations returned from the dataloader and not the
+        extracted features from the sensor module.
+
+        Args:
+            raw_observation: The raw observations from the dataloader
+            view_sensor_id: The ID of the view sensor
+            target_semantic_id: The semantic ID of the primary target object in the
+                scene.
+            multiple_objects_present: Whether there are multiple objects present in the
+                scene. If so, we do additional checks to make sure we don't get too
+                close to these when moving forward
+
+        Returns:
+            Tuple[Union[Action, None], bool]: The next action to take and whether the
+                episode is done.
+
+        Raises:
+            ValueError: If the object is not visible
+        """
+        # Reconstruct 2D semantic map.
+        depth_image = raw_observation[self.agent_id][view_sensor_id]["depth"]
+        semantic_3d = raw_observation[self.agent_id][view_sensor_id]["semantic_3d"]
+        semantic_image = semantic_3d[:, 3].reshape(depth_image.shape).astype(int)
+
+        if not multiple_objects_present:
+            semantic_image[semantic_image > 0] = target_semantic_id
+
+        points_on_target_obj = semantic_image == target_semantic_id
+        n_points_on_target_obj = points_on_target_obj.sum()
+
+        # For multi-object experiments, handle the possibility that object is no
+        # longer visible.
+        if multiple_objects_present and n_points_on_target_obj == 0:
+            logging.debug("Object not visible, cannot move closer")
+            return None, True
+
+        if n_points_on_target_obj > 0:
+            closest_point_on_target_obj = np.min(depth_image[points_on_target_obj])
+            logging.debug(
+                "closest target object point: " + str(closest_point_on_target_obj)
+            )
+        else:
+            raise ValueError(
+                "May be initializing experiment with no visible target object"
+            )
+
+        perc_on_target_obj = get_perc_on_obj_semantic(
+            semantic_image, semantic_id=target_semantic_id
+        )
+        logging.debug("% on target object: " + str(perc_on_target_obj))
+
+        # If the object touches outer pixels, we are close enough.
+        edge_buffer_pct = 5
+        edge_buffer = int(edge_buffer_pct / 100 * semantic_image.shape[0])
+        if semantic_image[:edge_buffer, :].sum() > 0:  # top side
+            return None, True
+        elif semantic_image[-edge_buffer:, :].sum() > 0:  # bottom side
+            return None, True
+        elif semantic_image[:, :edge_buffer].sum() > 0:  # left side
+            return None, True
+        elif semantic_image[:, -edge_buffer:].sum() > 0:  # right side
+            return None, True
+
+        # Also calculate closest point on *any* object so that we don't get too close
+        # and clip into objects; NB that any object will have a semantic ID > 0
+        points_on_any_obj = semantic_image > 0
+        closest_point_on_any_obj = np.min(depth_image[points_on_any_obj])
+        logging.debug("closest point on any object: " + str(closest_point_on_any_obj))
+
+        if perc_on_target_obj < self.good_view_percentage:
+            if closest_point_on_target_obj > self.desired_object_distance:
+                if multiple_objects_present and (
+                    closest_point_on_any_obj < self.desired_object_distance / 4
+                ):
+                    logging.debug(
+                        "getting too close to other objects, not moving forward"
+                    )
+                    return None, True  # done
+                else:
+                    logging.debug("move forward")
+                    return MoveForward(agent_id=self.agent_id, distance=0.005), False
+            else:
+                logging.debug("close enough")
+                return None, True  # done
+        else:
+            logging.debug("Enough percent visible")
+            return None, True  # done
+
+
+"""
+Configs
+---------
+"""
+
+motor_system_config = MotorSystemConfigInformedNoTransStepS20(
+    motor_system_class=FramedObjectPolicy,
+    motor_system_args=make_informed_policy_config(
+        action_space_type="distant_agent_no_translation",
+        action_sampler_class=ConstantSampler,
+        rotation_degrees=5.0,
+        use_goal_state_driven_actions=False,
+        switch_frequency=1.0,
+        good_view_percentage=0.5,  # Make sure we define the required
+        desired_object_distance=0.2,
+    ),
+)
+
+# The config dictionary for the standard experiment with 14 standard training rotations.
+view_finder_base = dict(
+    # Set up experiment
+    experiment_class=MontyObjectRecognitionExperiment,
+    experiment_args=EvalExperimentArgs(
+        model_name_or_path=model_path,  # load the pre-trained models from this path
+        n_eval_epochs=len(train_rotations),
+        max_eval_steps=1,
+        max_total_steps=1,
+    ),
+    logging_config=EvalLoggingConfig(
+        output_dir=output_dir,
+        run_name="view_finder_base",
+        monty_handlers=[ViewFinderRGBDHandler],
+        wandb_handlers=[],
+    ),
+    # Set up monty, including LM, SM, and motor system.
+    monty_config=PatchAndViewMontyConfig(
+        monty_args=MontyArgs(num_exploratory_steps=1),
+        motor_system_config=motor_system_config,
+    ),
+    # Set up environment/data
+    dataset_class=ED.EnvironmentDataset,
+    dataset_args=PatchViewFinderMountHabitatDatasetArgs(),
+    # dataset_args=dataset_args,
+    eval_dataloader_class=ED.InformedEnvironmentDataLoader,
+    eval_dataloader_args=EnvironmentDataloaderPerObjectArgs(
+        object_names=object_names,
+        object_init_sampler=PredefinedObjectInitializer(
+            positions=[[0.0, 1.5, -0.2]], rotations=train_rotations
+        ),
+    ),
+    # Doesn't get used, but currently needs to be set anyways.
+    train_dataloader_class=ED.InformedEnvironmentDataLoader,
+    train_dataloader_args=EnvironmentDataloaderPerObjectArgs(
+        object_names=object_names,
+        object_init_sampler=PredefinedObjectInitializer(rotations=train_rotations),
+    ),
+)
+# Set viewfinder resolution to 224 x 224
+dataset_args = view_finder_base["dataset_args"]
+dataset_args.env_init_args["agents"][0].agent_args["resolutions"] = [
+    [64, 64],
+    [224, 224],
+]
+dataset_args.__post_init__()
+
+"""
+14 Randomly Generated Rotations
+-------------------------------
+"""
+view_finder_randrot_14 = copy.deepcopy(view_finder_base)
+view_finder_randrot_14["experiment_args"].n_eval_epochs = 14
+view_finder_randrot_14["logging_config"].run_name = "view_finder_randrot_14"
+view_finder_randrot_14[
+    "eval_dataloader_args"
+].object_init_sampler = RandomRotationObjectInitializer()
+
+"""
+5 (Pre-defined) Random Rotations
+--------------------------------
+"""
+view_finder_randrot_5 = copy.deepcopy(view_finder_base)
+view_finder_randrot_5["experiment_args"].n_eval_epochs = 5
+view_finder_randrot_5["logging_config"].run_name = "view_finder_randrot_5"
+view_finder_randrot_5[
+    "eval_dataloader_args"
+].object_init_sampler = PredefinedObjectInitializer(
+    positions=[[0.0, 1.5, -0.2]], rotations=random_rotations_5
+)
+
+CONFIGS = {
+    "view_finder_base": view_finder_base,
+    "view_finder_randrot_14": view_finder_randrot_14,
+    "view_finder_randrot_5": view_finder_randrot_5,
+}
---
+++ dmc_configs/scripts/render_view_finder_images.py
@@ -0,0 +1,100 @@
+# %%
+import json
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+experiment = "view_finder_randrot_5"
+
+figure_settings = {
+    "view_finder_base": {
+        "n_rotations": 14,
+        "n_rows": 2,
+        "n_cols": 7,
+        "figsize": (8, 4),
+    },
+    "view_finder_randrot_14": {
+        "n_rotations": 14,
+        "n_rows": 2,
+        "n_cols": 7,
+        "figsize": (8, 4),
+    },
+    "view_finder_randrot_5": {
+        "n_rotations": 5,
+        "n_rows": 1,
+        "n_cols": 5,
+        "figsize": (4, 4),
+    },
+}
+
+project_dir = Path("~/tbp/results/monty/projects/view_finder_images").expanduser()
+data_dir = project_dir / f"{experiment}/view_finder_rgbd"
+arrays_dir = data_dir / "arrays"
+visualization_dir = data_dir / "visualizations"
+visualization_dir.mkdir(parents=True, exist_ok=True)
+
+# Load episodes.json to get object ids and rotations.
+# Items are tuples of (episode_num, object_name, rotation)
+episodes = []
+with open(os.path.join(data_dir, "episodes.jsonl"), "r") as f:
+    for line in f:
+        episode = json.loads(line)
+        episode_num = episode["episode"]
+        object_name = episode["object"]
+        rotation = episode["rotation"]
+        episodes.append((episode_num, object_name, rotation))
+
+# Let's get unique rotations
+rotations = [tuple(episode[2]) for episode in episodes]
+unique_rotations = set(rotations)
+
+# Let's get unique objects so we can create 4 x 8 grids for each object.
+objects = [episode[1] for episode in episodes]
+unique_objects = set(objects)
+
+
+def _get_episodes_for_object(object_name, episodes):
+    # get all episodes for the given object sorted be episode number
+    episodes = [episode for episode in episodes if episode[1] == object_name]
+    # sort the episodes by rotations
+    episodes = sorted(episodes, key=lambda x: x[2])
+    return episodes
+
+
+def _plot_all_rotations_for_object(
+    object_name, episodes, visualization_dir, fig_params
+):
+    figsize = fig_params["figsize"]
+    n_rows, n_cols = fig_params["n_rows"], fig_params["n_cols"]
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize)
+    for i, episode in enumerate(episodes):
+        episode_number = episode[0]
+        rotation = episode[2]
+        image_rgbd = np.load(os.path.join(data_dir, f"arrays/{episode_number}.npy"))
+        image_rgb = image_rgbd[:, :, :3]
+        ax = axes.flatten()[i]
+        ax.imshow(image_rgb)
+        # Title with the rotation
+        title = "[{:d}, {:d}, {:d}]".format(*map(round, rotation))
+        ax.set_title(title, fontsize=6)
+        # remove ticks
+        ax.set_xticks([])
+        ax.set_yticks([])
+    fig.tight_layout()
+    plt.savefig(
+        os.path.join(visualization_dir, f"{object_name}.png"),
+        bbox_inches="tight",
+        dpi=300,
+    )
+    plt.close()
+
+for object_name in tqdm.tqdm(unique_objects):
+    object_episodes = _get_episodes_for_object(object_name, episodes)
+    fig_params = figure_settings[experiment]
+    _plot_all_rotations_for_object(
+        object_name, object_episodes, visualization_dir, fig_params
+    )
+
