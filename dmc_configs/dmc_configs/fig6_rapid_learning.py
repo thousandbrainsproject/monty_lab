9d1661a86ceb3744381cc6a9ad3c834bbdfb51e1
Scott Knudstrup <scottknudstrup@gmail.com>
2025-01-20 22:30:26 -0800

    Update fig6_rapid_learning.py

--- dmc_configs/dmc_configs/fig6_rapid_learning.py
+++ dmc_configs/dmc_configs/fig6_rapid_learning.py
@@ -1,19 +1,67 @@
 import copy
+import time
 from pathlib import Path
 
 import torch
-
-from dmc_configs.pretraining_experiments import (
-    MontySupervisedObjectPretrainingExperiment,
+from tbp.monty.frameworks.config_utils.config_args import (
     PretrainLoggingConfig,
 )
+from tbp.monty.frameworks.config_utils.make_dataset_configs import (
+    EnvironmentDataloaderPerObjectArgs,
+    ExperimentArgs,
+    PredefinedObjectInitializer,
+)
+from tbp.monty.frameworks.environments import embodied_data as ED
+from tbp.monty.frameworks.environments.ycb import SHUFFLED_YCB_OBJECTS
+from tbp.monty.frameworks.experiments.pretraining_experiments import (
+    MontySupervisedObjectPretrainingExperiment,
+)
 
+from .common import PRETRAIN_DIR
 from .pretraining_experiments import pretrain_dist_agent_1lm
 
 """
-Define pretraining experiment(s)
+Rapid Learning Config (for storing checkpoints)
+--------------------------------------------------------------------------------
 """
 
+TRAIN_ROTATIONS = [
+    # cube faces
+    [0, 0, 0],
+    [0, 90, 0],
+    [0, 180, 0],
+    [0, 270, 0],
+    [90, 0, 0],
+    [90, 180, 0],
+    # cube corners
+    [35, 45, 0],
+    [325, 45, 0],
+    [35, 315, 0],
+    [325, 315, 0],
+    [35, 135, 0],
+    [325, 135, 0],
+    [35, 225, 0],
+    [325, 225, 0],
+    # random rotations (numpy.random.randint)
+    [305, 143, 316],
+    [63, 302, 307],
+    [286, 207, 136],
+    [164, 2, 181],
+    [276, 68, 121],
+    [114, 88, 272],
+    [152, 206, 301],
+    [242, 226, 282],
+    [235, 321, 32],
+    [33, 243, 166],
+    [65, 298, 9],
+    [185, 14, 224],
+    [259, 249, 53],
+    [113, 8, 73],
+    [20, 158, 74],
+    [289, 327, 94],
+    [148, 181, 282],
+    [240, 143, 10],
+]
 
 class PretrainingExperimentWithCheckpointing(
     MontySupervisedObjectPretrainingExperiment
@@ -35,31 +83,43 @@ class PretrainingExperimentWithCheckpointing(
         super().post_epoch()
 
         # Check which epooch?
+        if self.train_epochs == 1:
+            self.t_last_checkpoint = time.time()
+        else:
+            t_per_epoch = time.time() - self.t_last_checkpoint
+            mins, secs = divmod(t_per_epoch, 60)
+            print(f"Time per epoch: {mins:.2f} minutes, {secs:.2f} seconds")
+            self.t_last_checkpoint = time.time()
 
         # Save the model.
-        output_dir = Path(self.output_dir) / f"checkpoints/{self.train_epochs}"
+        checkpoints_dir = Path(self.output_dir) / "checkpoints"
+        # checkpoints_dir = Path(self.output_dir).parent / "checkpoints"
+        output_dir = checkpoints_dir / f"{self.train_epochs}"
         output_dir.mkdir(parents=True, exist_ok=True)
         model_path = output_dir / "model.pt"
         model_state_dict = self.model.state_dict()
         torch.save(model_state_dict, model_path)
 
 
-"""
-Rapid Learning Config (for storing checkpoints)
---------------------------------------------------------------------------------
-"""
-
 pretrain_dist_agent_1lm_checkpoints = copy.deepcopy(pretrain_dist_agent_1lm)
-pretrain_dist_agent_1lm_checkpoints["experiment_class"] = (
-    PretrainingExperimentWithCheckpointing
+pretrain_dist_agent_1lm_checkpoints.update(
+    dict(
+        experiment_class=PretrainingExperimentWithCheckpointing,
+        experiment_args=ExperimentArgs(
+            n_train_epochs=len(TRAIN_ROTATIONS),
+            do_eval=False,
+        ),
+        logging_config=PretrainLoggingConfig(
+            output_dir=str(PRETRAIN_DIR),
+            run_name="dist_agent_1lm_checkpoints",
+        ),
+        train_dataloader_class=ED.InformedEnvironmentDataLoader,
+        train_dataloader_args=EnvironmentDataloaderPerObjectArgs(
+            object_names=SHUFFLED_YCB_OBJECTS,
+            object_init_sampler=PredefinedObjectInitializer(rotations=TRAIN_ROTATIONS),
+        ),
+    )
 )
-pretrain_dist_agent_1lm_checkpoints["logging_config"] = PretrainLoggingConfig(
-    run_name="dist_agent_1lm_checkpoints"
-)
-
-# pretrain_dist_agent_1lm_checkpoints[
-#     "train_dataloader_args"
-# ].object_names = DISTINCT_OBJECTS
 
 CONFIGS = {
     "pretrain_dist_agent_1lm_checkpoints": pretrain_dist_agent_1lm_checkpoints,
