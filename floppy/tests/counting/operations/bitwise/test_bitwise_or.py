000d1c631033e79b374443e37c530138982b4d17
hlee9212 <hlee@numenta.com>
2025-01-23 13:14:08 -0800

    Add bitwise_or tests

--- floppy/tests/counting/operations/bitwise/test_bitwise_or.py
+++ floppy/tests/counting/operations/bitwise/test_bitwise_or.py
@@ -1,54 +1,52 @@
-"""Run using python tests/test_bitwise_or.py. Do not use pytest."""
-
 import numpy as np
-from floppy.flop_counting.counter import FlopCounter
+import pytest
+
+from floppy.counting.counter import FlopCounter
 
 
 def test_bitwise_or_operator_syntax():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([1, 2, 3])
         b = np.array([4, 5, 6])
-        _ = a | b
+        result = a | b
         assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([5, 7, 7]))
 
 
 def test_bitwise_or_ufunc_syntax():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([1, 2, 3])
         b = np.array([4, 5, 6])
-        _ = np.bitwise_or(a, b)
+        result = np.bitwise_or(a, b)
         assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([5, 7, 7]))
 
 
 def test_bitwise_or_augmented_assignment():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([1, 2, 3])
         b = np.array([4, 5, 6])
         a |= b
         assert counter.flops == 3
+        np.testing.assert_array_equal(a, np.array([5, 7, 7]))
 
 
 def test_bitwise_or_broadcasting():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([1, 2, 3])
         b = 2
-        _ = a | b
+        result = a | b
         assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([3, 2, 3]))
 
     counter.flops = 0
     with counter:
         a = np.array([1, 2, 3])
         b = 2
-        _ = b | a
+        result = b | a
         assert counter.flops == 3
-
-
-if __name__ == "__main__":
-    test_bitwise_or_operator_syntax()
-    test_bitwise_or_ufunc_syntax()
-    test_bitwise_or_augmented_assignment()
-    test_bitwise_or_broadcasting()
+        np.testing.assert_array_equal(result, np.array([3, 2, 3]))
