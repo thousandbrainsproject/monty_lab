4f22a4071218eca02a7e57e4a35b195fc3aa9692
hlee9212 <hlee@numenta.com>
2025-01-23 12:13:33 -0800

    Simplify test_add to use functions only

--- floppy/tests/counting/operations/arithmetic/test_add.py
+++ floppy/tests/counting/operations/arithmetic/test_add.py
@@ -4,65 +4,58 @@ import pytest
 from floppy.counting.counter import FlopCounter
 
 
-class TestAdd:
-    def setup_method(self):
-        self.original_array = np.array
+def test_add_operator_syntax():
+    counter = FlopCounter(test_mode=True)
 
-    def teardown_method(self):
-        np.array = self.original_array
+    with counter:
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+        result = a + b
+        assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([5, 7, 9]))
 
-    def test_add_operator_syntax(self):
-        counter = FlopCounter(test_mode=True)
+def test_add_ufunc_syntax():
+    counter = FlopCounter(test_mode=True)
+    with counter:
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+        result = np.add(a, b)
+        assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([5, 7, 9]))
 
-        with counter:
-            a = np.array([1, 2, 3])
-            b = np.array([4, 5, 6])
-            result = a + b
-            assert counter.flops == 3
-            np.testing.assert_array_equal(result, np.array([5, 7, 9]))
+# FIXME
+@pytest.mark.xfail(reason="TrackedArray object has no attribute 'add'")
+def test_add_method_syntax():
+    counter = FlopCounter(test_mode=True)
+    with counter:
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+        result = a.add(b)
+        assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([5, 7, 9]))
 
-    def test_add_ufunc_syntax(self):
-        counter = FlopCounter(test_mode=True)
-        with counter:
-            a = np.array([1, 2, 3])
-            b = np.array([4, 5, 6])
-            result = np.add(a, b)
-            assert counter.flops == 3
-            np.testing.assert_array_equal(result, np.array([5, 7, 9]))
+def test_add_augmented_assignment():
+    counter = FlopCounter(test_mode=True)
+    with counter:
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+        a += b
+        assert counter.flops == 3
+        np.testing.assert_array_equal(a, np.array([5, 7, 9]))
 
-    # FIXME
-    @pytest.mark.xfail(reason="TrackedArray object has no attribute 'add'")
-    def test_add_method_syntax(self):
-        counter = FlopCounter(test_mode=True)
-        with counter:
-            a = np.array([1, 2, 3])
-            b = np.array([4, 5, 6])
-            result = a.add(b)
-            assert counter.flops == 3
-            np.testing.assert_array_equal(result, np.array([5, 7, 9]))
+def test_add_broadcasting():
+    counter = FlopCounter(test_mode=True)
+    with counter:
+        a = np.array([1, 2, 3])
+        b = 2
+        result = a + b
+        assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([3, 4, 5]))
 
-    def test_add_augmented_assignment(self):
-        counter = FlopCounter(test_mode=True)
-        with counter:
-            a = np.array([1, 2, 3])
-            b = np.array([4, 5, 6])
-            a += b
-            assert counter.flops == 3
-            np.testing.assert_array_equal(a, np.array([5, 7, 9]))
-
-    def test_add_broadcasting(self):
-        counter = FlopCounter(test_mode=True)
-        with counter:
-            a = np.array([1, 2, 3])
-            b = 2
-            result = a + b
-            assert counter.flops == 3
-            np.testing.assert_array_equal(result, np.array([3, 4, 5]))
-
-        counter.flops = 0
-        with counter:
-            a = np.array([1, 2, 3])
-            b = 2
-            result = b + a
-            assert counter.flops == 3
-            np.testing.assert_array_equal(result, np.array([3, 4, 5]))
+    counter.flops = 0
+    with counter:
+        a = np.array([1, 2, 3])
+        b = 2
+        result = b + a
+        assert counter.flops == 3
+        np.testing.assert_array_equal(result, np.array([3, 4, 5]))
