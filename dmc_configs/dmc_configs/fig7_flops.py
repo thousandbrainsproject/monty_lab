617e8a46cbfe20757dbd4c51ce8265947dc78beb
hlee9212 <hlee@numenta.com>
2025-01-22 18:30:00 -0800

    Refactor configuration updates in fig7_flops.py
    - Updated the `update_x_percent_threshold_in_config` function to modify the x_percent threshold in-place and clarified its arguments in the docstring.
    - Replaced references to `dist_agent_1lm_nohyp_randrot` with `dist_agent_1lm_randrot_noise_nohyp` for consistency.
    - Adjusted x_percent thresholds for configurations (5p, 10p, 20p, 30p) to use updated values.
    - Added a new CONFIGS dictionary to consolidate the updated configurations for easier access.

--- dmc_configs/dmc_configs/fig7_flops.py
+++ dmc_configs/dmc_configs/fig7_flops.py
@@ -27,17 +27,20 @@ Experiments use:
 """
 
 import copy
-from dmc_configs.dmc_eval_experiments import dist_agent_1lm_nohyp_randrot
+from .fig4_rapid_inference_with_voting import dist_agent_1lm_randrot_noise_nohyp
 
 
 def update_x_percent_threshold_in_config(
     config, x_percent_threshold, evidence_update_threshold="x_percent_threshold"
 ):
     """Update the x_percent threshold in the config.
+    This function modifies the config in-place.
 
     Args:
         config (dict): The config to update.
         x_percent_threshold (float): The percentage of the threshold to update.
+        evidence_update_threshold (str): How to decide which hypotheses should be updated.
+            In [int, float, 'mean', 'median', 'all', 'x_percent_threshold'].
 
     Returns:
         dict: The updated config.
@@ -50,7 +53,7 @@ def update_x_percent_threshold_in_config(
     # Update the x_percent_threshold
     lm_config_dict = config["monty_config"].learning_module_configs
     lm_config_dict["learning_module_0"]["learning_module_args"][
-        "evidence_update_threshold"
+        "x_percent_threshold"
     ] = x_percent_threshold
 
     # Update the string value for evidence_update_threshold
@@ -60,33 +63,56 @@ def update_x_percent_threshold_in_config(
     return config
 
 
-dist_agent_1lm_randrot_nohyp_x_percent_5p = copy.deepcopy(dist_agent_1lm_nohyp_randrot)
+dist_agent_1lm_randrot_no_noise_nohyp = copy.deepcopy(
+    dist_agent_1lm_randrot_noise_nohyp
+)
+# TODO: Remove sensor noise
+# Or can I start from fig6's dist_agent_1lm_ranrot_nohyp_1rot_trained? (Though I think I need 14 rot?)
+
+
+dist_agent_1lm_randrot_nohyp_x_percent_5p = copy.deepcopy(
+    dist_agent_1lm_randrot_noise_nohyp
+)
 dist_agent_1lm_randrot_nohyp_x_percent_5p = update_x_percent_threshold_in_config(
-    dist_agent_1lm_randrot_nohyp_x_percent_5p, 0.05, "x_percent_threshold"
+    dist_agent_1lm_randrot_nohyp_x_percent_5p, 5, "x_percent_threshold"
 )
 
-dist_agent_1lm_randrot_nohyp_x_percent_10p = copy.deepcopy(dist_agent_1lm_nohyp_randrot)
+dist_agent_1lm_randrot_nohyp_x_percent_10p = copy.deepcopy(
+    dist_agent_1lm_randrot_noise_nohyp
+)
 dist_agent_1lm_randrot_nohyp_x_percent_10p = update_x_percent_threshold_in_config(
-    dist_agent_1lm_randrot_nohyp_x_percent_10p, 0.1, "x_percent_threshold"
+    dist_agent_1lm_randrot_nohyp_x_percent_10p, 10, "x_percent_threshold"
 )
 
-dist_agent_1lm_randrot_nohyp_x_percent_20p = copy.deepcopy(dist_agent_1lm_nohyp_randrot)
+dist_agent_1lm_randrot_nohyp_x_percent_20p = copy.deepcopy(
+    dist_agent_1lm_randrot_noise_nohyp
+)
 dist_agent_1lm_randrot_nohyp_x_percent_20p = update_x_percent_threshold_in_config(
-    dist_agent_1lm_randrot_nohyp_x_percent_20p, 0.2, "x_percent_threshold"
+    dist_agent_1lm_randrot_nohyp_x_percent_20p, 20, "x_percent_threshold"
 )
 
-dist_agent_1lm_randrot_nohyp_x_percent_30p = copy.deepcopy(dist_agent_1lm_nohyp_randrot)
+dist_agent_1lm_randrot_nohyp_x_percent_30p = copy.deepcopy(
+    dist_agent_1lm_randrot_noise_nohyp
+)
 dist_agent_1lm_randrot_nohyp_x_percent_30p = update_x_percent_threshold_in_config(
-    dist_agent_1lm_randrot_nohyp_x_percent_30p, 0.3, "x_percent_threshold"
+    dist_agent_1lm_randrot_nohyp_x_percent_30p, 30, "x_percent_threshold"
 )
 
 dist_agent_1lm_randrot_nohyp_x_percent_30p_evidence_update_all = copy.deepcopy(
-    dist_agent_1lm_nohyp_randrot
+    dist_agent_1lm_randrot_noise_nohyp
 )
 dist_agent_1lm_randrot_nohyp_x_percent_30p_evidence_update_all = (
     update_x_percent_threshold_in_config(
         dist_agent_1lm_randrot_nohyp_x_percent_30p_evidence_update_all,
-        0.3,
+        30,
         "all",
     )
 )
+
+CONFIGS = {
+    "dist_agent_1lm_randrot_nohyp_x_percent_5p": dist_agent_1lm_randrot_nohyp_x_percent_5p,
+    "dist_agent_1lm_randrot_nohyp_x_percent_10p": dist_agent_1lm_randrot_nohyp_x_percent_10p,
+    "dist_agent_1lm_randrot_nohyp_x_percent_20p": dist_agent_1lm_randrot_nohyp_x_percent_20p,
+    "dist_agent_1lm_randrot_nohyp_x_percent_30p": dist_agent_1lm_randrot_nohyp_x_percent_30p,
+    "dist_agent_1lm_randrot_nohyp_x_percent_30p_evidence_update_all": dist_agent_1lm_randrot_nohyp_x_percent_30p_evidence_update_all,
+}
