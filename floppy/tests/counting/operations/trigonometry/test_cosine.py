90008b1bae813e57177a6ef2e9c2ddce31f83936
hlee9212 <hlee@numenta.com>
2025-01-23 20:32:31 -0800

    Add tests for np.cos.

--- floppy/tests/counting/operations/trigonometry/test_cosine.py
+++ floppy/tests/counting/operations/trigonometry/test_cosine.py
@@ -1,40 +1,42 @@
-"""Run using python tests/test_cosine.py. Do not use pytest."""
-
 import numpy as np
-from floppy.flop_counting.counter import FlopCounter
+import pytest
+
+from floppy.counting.counter import FlopCounter
 
 
 def test_cos_ufunc_syntax():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([1, 2, 3])
-        _ = np.cos(a)
-        assert counter.flops == 24  # 8 flops * 3 elements
+        result = np.cos(a)
+        assert counter.flops == 60  # 20 flops * 3 elements
+        np.testing.assert_allclose(
+            result, np.array([0.54030231, -0.41614684, -0.9899925])
+        )
 
 
 def test_cos_broadcasting():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = 2
-        _ = np.cos(a)
-        assert counter.flops == 8  # 8 flops * 1 element
+        result = np.cos(a)
+        assert counter.flops == 20
+        np.testing.assert_allclose(result, -0.41614684)
 
     counter.flops = 0
     with counter:
         a = np.array([[1, 2], [3, 4]])
-        _ = np.cos(a)
-        assert counter.flops == 32  # 8 flops * 4 elements
+        result = np.cos(a)
+        assert counter.flops == 80
+        np.testing.assert_allclose(
+            result, np.array([[0.54030231, -0.41614684], [-0.9899925, -0.65364362]])
+        )
 
 
 def test_cos_empty():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([])
-        _ = np.cos(a)
+        result = np.cos(a)
         assert counter.flops == 0
-
-
-if __name__ == "__main__":
-    test_cos_ufunc_syntax()
-    test_cos_broadcasting()
-    test_cos_empty()
+        np.testing.assert_allclose(result, np.array([]))
