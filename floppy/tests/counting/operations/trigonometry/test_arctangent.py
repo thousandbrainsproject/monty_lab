cad378a3fda96e621e506ad7a18d9d4d9fb9abef
hlee9212 <hlee@numenta.com>
2025-01-23 21:41:57 -0800

    Add tests for np.arctan.

--- floppy/tests/counting/operations/trigonometry/test_arctangent.py
+++ floppy/tests/counting/operations/trigonometry/test_arctangent.py
@@ -1,40 +1,45 @@
 """Run using python tests/test_arctangent.py. Do not use pytest."""
 
 import numpy as np
-from floppy.flop_counting.counter import FlopCounter
+
+from floppy.counting.counter import FlopCounter
 
 
 def test_arctan_ufunc_syntax():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = np.array([1, 2, 3])
-        _ = np.arctan(a)
-        assert counter.flops == 30  # 10 flops * 3 elements
+        result = np.arctan(a)
+        assert counter.flops == 60
+        np.testing.assert_allclose(
+            result, np.array([0.78539816, 1.10714872, 1.24904577])
+        )
 
 
 def test_arctan_broadcasting():
-    counter = FlopCounter()
+    counter = FlopCounter(test_mode=True)
     with counter:
         a = 2
-        _ = np.arctan(a)
-        assert counter.flops == 10  # 10 flops * 1 element
+        result = np.arctan(a)
+        assert counter.flops == 20
+        np.testing.assert_allclose(result, np.array([1.10714872]))
 
     counter.flops = 0
     with counter:
         a = np.array([[1, 2], [3, 4]])
-        _ = np.arctan(a)
-        assert counter.flops == 40  # 10 flops * 4 elements
+        result = np.arctan(a)
+        assert counter.flops == 80
+        np.testing.assert_allclose(
+            result, np.array([[0.78539816, 1.10714872], [1.24904577, 1.32581766]])
+        )
 
 
 def test_arctan_empty():
     counter = FlopCounter()
     with counter:
         a = np.array([])
-        _ = np.arctan(a)
+        result = np.arctan(a)
         assert counter.flops == 0
+        np.testing.assert_allclose(result, np.array([]))
 
 
-if __name__ == "__main__":
-    test_arctan_ufunc_syntax()
-    test_arctan_broadcasting()
-    test_arctan_empty()
