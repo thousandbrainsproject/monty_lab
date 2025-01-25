077d0d5ce4ed80fc436bb4dfbaa2f999dd514f11
Scott Knudstrup <scottknudstrup@gmail.com>
2025-01-24 21:52:25 -0800

    Update fig6_rapid_learning.py

--- dmc/configs/fig6_rapid_learning.py
+++ dmc/configs/fig6_rapid_learning.py
@@ -9,7 +9,8 @@
 # https://opensource.org/licenses/MIT.
 """Figure 6: Rapid Learning
 
-Consists of 6 experiments:
+Consists one pretraining experiment and 6 evaluation experiments:
+- pretrain_dist_agent_1lm_checkpoints
 - dist_agent_1lm_randrot_nohyp_1rot_trained
 - dist_agent_1lm_randrot_nohyp_2rot_trained
 - dist_agent_1lm_randrot_nohyp_4rot_trained
