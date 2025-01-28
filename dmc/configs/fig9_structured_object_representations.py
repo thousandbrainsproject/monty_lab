90cc0b381e856df4ba210f9e46af08fce1b5fc53
Scott Knudstrup <scottknudstrup@gmail.com>
2025-01-27 18:48:35 -0800

    Update fig9_structured_object_representations.py

--- dmc/configs/fig9_structured_object_representations.py
+++ dmc/configs/fig9_structured_object_representations.py
@@ -24,23 +24,23 @@ This module defines the following experiments:
 
 import copy
 
-from tbp.monty.frameworks.config_utils.config_args import EvalEvidenceLMLoggingConfig
+from tbp.monty.frameworks.config_utils.config_args import (
+    DetailedEvidenceLMLoggingConfig,
+    EvalEvidenceLMLoggingConfig,
+)
 from tbp.monty.frameworks.environments.ycb import SIMILAR_OBJECTS
 
 from .common import DMC_RESULTS_DIR
 from .fig4_rapid_inference_with_voting import dist_agent_1lm_randrot_noise
 
-# logging_config=EvalEvidenceLMLoggingConfig(
-# monty_handlers=[BasicCSVStatsHandler, DetailedJSONHandler],
-# wandb_handlers=[],
-# monty_log_level="SELECTIVE",
-# ),
 dist_agent_1lm_randrot_noise_10simobj = copy.deepcopy(dist_agent_1lm_randrot_noise)
-dist_agent_1lm_randrot_noise_10simobj["logging_config"] = EvalEvidenceLMLoggingConfig(
-    output_dir=DMC_RESULTS_DIR / "dist_agent_1lm_randrot_noise_10simobj",
-    run_name="dist_agent_1lm_randrot_noise_10simobj",
-    wandb_group="dmc",
-    monty_log_level="SELECTIVE",
+dist_agent_1lm_randrot_noise_10simobj["logging_config"] = (
+    DetailedEvidenceLMLoggingConfig(
+        output_dir=str(DMC_RESULTS_DIR),
+        run_name="dist_agent_1lm_randrot_noise_10simobj",
+        wandb_group="dmc",
+        monty_log_level="SELECTIVE",
+    )
 )
 dist_agent_1lm_randrot_noise_10simobj[
     "eval_dataloader_args"
