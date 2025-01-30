8dce8289d9a5b0baff647e5d6fda98d8ec58066f
Scott Knudstrup <scottknudstrup@gmail.com>
2025-01-29 23:39:41 -0800

    Update evidence_counts.py

--- dmc/scripts/evidence_counts.py
+++ dmc/scripts/evidence_counts.py
@@ -37,15 +37,24 @@ def describe_dict(data: Mapping, level: int = 0):
 class DetailedJSONStatsInterface:
     """ "Convenience interface to detailed JSON stats.
 
-    Any episodes can be loaded via `read_episode` or (equivalently) `__getitem__`,
-    but there is overhead involved. If you plan on loading data from many episodes
-    in a row, the most efficient method is to iterate over `DetailedJSONStatsInterface`.
+    This class is a dict-like interface to detailed JSON stats files that loads
+    episodes one at a time. An episode can be loaded via `stats[episode_num]`
+    (or, equivalently `stats.read_episode(episode_num)`), which takes about
+    1.5 - 6.5 seconds per episode. If you plan on loading all episodes eventually,
+    the most efficient method is to iterate over a `DetailedJSONStatsInterface`.
+
+    Example:
+        >>> stats = DetailedJSONStatsInterface("detailed_stats.json")
+        >>> last_episode_data = stats[-1]  # Get data for the last episode.
+        >>> # Iterate over all episodes.
+        >>> for i, episode_data in enumerate(stats):
+        ...     # Process episode data
+        ...     pass
     """
 
     def __init__(self, path: os.PathLike):
         self._path = Path(path)
         self._index = None  # Just used to convert possibly negative indices
-        self._initialized = False
 
     @property
     def path(self) -> os.PathLike:
@@ -61,16 +70,14 @@ class DetailedJSONStatsInterface:
                     return json.loads(line)[str(i)]
 
     def _check_initialized(self):
-        if self._initialized:
+        if self._index is not None:
             return
         length = 0
         with open(self._path, "r") as f:
             length = sum(1 for _ in f)
         self._index = np.arange(length)
-        self._initialized = True
 
     def __iter__(self):
-        self._check_initialized()
         with open(self._path, "r") as f:
             for i, line in enumerate(f):
                 yield json.loads(line)[str(i)]
@@ -91,6 +98,6 @@ class DetailedJSONStatsInterface:
         return self.read_episode(episode)
 
 
+
 experiment_dir = DMC_RESULTS_DIR / "dist_agent_1lm_randrot_noise_10simobj"
-json_stats = DetailedJSONStatsInterface(experiment_dir / "detailed_run_stats.json")
-ep = json_stats[0]
+stats = DetailedJSONStatsInterface(experiment_dir / "detailed_run_stats.json")
