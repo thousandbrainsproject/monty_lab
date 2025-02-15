1651d2fa0e329560dead91ac4d6694e4e4f83394
Scott Knudstrup <scottknudstrup@gmail.com>
2025-02-14 16:04:12 -0800

    Add to data_utils.py and plot_utils.py

--- dmc/scripts/plot_utils.py
+++ dmc/scripts/plot_utils.py
@@ -16,6 +16,8 @@ from numbers import Number
 from typing import Any, List, Optional
 
 import matplotlib.axes
+import numpy as np
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 TBP_COLORS = {
     "blue": "#00A0DF",
@@ -24,6 +26,76 @@ TBP_COLORS = {
     "green": "#008E42",
     "yellow": "#FFBE31",
 }
+
+
+def axes3d_clean(
+    ax: Axes3D,
+    grid: bool = True,
+    ticks: bool = False,
+    label_axes: bool = False,
+) -> None:
+    """Remove clutter from 3D axes.
+
+    Args:
+        grid (bool): Whether to show the background grid. Default is True.
+        ticks (bool): Whether to show the ticks that stick out the side of the
+            axes. Setting this to `False` does not remove grid lines. Default is False.
+        label_axes (bool): Whether to show the x, y, z axis labels. Default is False.
+    """
+
+    # # Turn grid on or off.
+    # ax.grid(grid)
+
+    # Remove dark spines that outline the plot.
+    ax.w_xaxis.line.set_color((1, 1, 1, 0))  # Hide X-axis line
+    ax.w_yaxis.line.set_color((1, 1, 1, 0))  # Hide Y-axis line
+    ax.w_zaxis.line.set_color((1, 1, 1, 0))  # Hide Z-axis line
+
+    # Optionally remove ticks. This method keeps the grid lines visible while
+    # making the stubs that stick out of the plot invisible.
+    if not ticks:
+        ax.tick_params(axis="x", colors=(0, 0, 0, 0))  # Make X-axis ticks invisible
+        ax.tick_params(axis="y", colors=(0, 0, 0, 0))  # Make Y-axis ticks invisible
+        ax.tick_params(axis="z", colors=(0, 0, 0, 0))  # Make Z-axis ticks invisible
+
+    # Optionally remove or add axis labels.
+    if label_axes:
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+    else:
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+        ax.set_zlabel("")
+
+    # Turn grid on or off.
+    ax.grid(grid)
+
+
+def axes3d_set_aspect_equal(ax: Axes3D) -> None:
+    """Set equal aspect ratio for 3D axes."""
+    x_limits = ax.get_xlim()
+    y_limits = ax.get_ylim()
+    z_limits = ax.get_zlim()
+
+    # Get the max range
+    x_range = x_limits[1] - x_limits[0]
+    y_range = y_limits[1] - y_limits[0]
+    z_range = z_limits[1] - z_limits[0]
+    half_max_range = max(x_range, y_range, z_range) / 2
+
+    # Find midpoints
+    x_middle = np.mean(x_limits)
+    y_middle = np.mean(y_limits)
+    z_middle = np.mean(z_limits)
+
+    # Set new limits
+    ax.set_xlim([x_middle - half_max_range, x_middle + half_max_range])
+    ax.set_ylim([y_middle - half_max_range, y_middle + half_max_range])
+    ax.set_zlim([z_middle - half_max_range, z_middle + half_max_range])
+
+    # Set aspect ratio.
+    ax.set_box_aspect([1, 1, 1])
 
 
 def violinplot(
