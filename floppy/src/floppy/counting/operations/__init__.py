039b94a6e6ef07b39547a09dbb1523092e9808c2
hlee9212 <hlee@numenta.com>
2025-03-13 08:40:11 -0700

    feat: add signal operations to counting module and update __all__ exports

--- floppy/src/floppy/counting/operations/__init__.py
+++ floppy/src/floppy/counting/operations/__init__.py
@@ -3,6 +3,17 @@ from .bitwise import *
 from .exponential import *
 from .linalg import *
 from .reduction import *
+from .signal import *
 from .special import *
 from .statistical import *
 from .trigonometry import *
+
+__all__ = (
+    arithmetic.__all__
+    + linalg.__all__
+    + reduction.__all__
+    + signal.__all__
+    + special.__all__
+    + statistical.__all__
+    + trigonometry.__all__
+)
