7cf5fced26a6f79ae59ca0f39824925e211215e4
Scott Knudstrup <scottknudstrup@gmail.com>
2025-03-14 04:04:28 -0700

    Update fig6.py

--- dmc/scripts/fig6.py
+++ dmc/scripts/fig6.py
@@ -33,6 +33,7 @@ from data_utils import (
     DetailedJSONStatsInterface,
     get_frequency,
     load_eval_stats,
+    load_object_model,
 )
 from matplotlib.lines import Line2D
 from plot_utils import TBP_COLORS, add_legend, axes3d_set_aspect_equal, violinplot
@@ -43,3 +44,30 @@ plt.rcParams["svg.fonttype"] = "none"
 
 OUT_DIR = DMC_ANALYSIS_DIR / "fig6"
 OUT_DIR.mkdir(parents=True, exist_ok=True)
+
+exp_dir = VISUALIZATION_RESULTS_DIR / "fig6_curvature_guided_policy"
+detailed_stats_path = exp_dir / "detailed_run_stats.json"
+detailed_stats_interface = DetailedJSONStatsInterface(detailed_stats_path)
+stats = detailed_stats_interface[0]
+# %%
+sm = stats["SM_0"]
+locations = np.array(
+    [obs["location"] for obs in stats["SM_0"]["processed_observations"]]
+)
+
+model = load_object_model("dist_agent_1lm", "mug")
+fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
+ax.scatter(model.x, model.y, model.z, color=model.rgba, alpha=0.1, s=10)
+
+ax.scatter(
+    locations[:, 0],
+    locations[:, 1],
+    locations[:, 2],
+    color=TBP_COLORS["blue"],
+    alpha=1,
+    s=10,
+    zorder=10,
+)
+plt.show()
+
+# %%
