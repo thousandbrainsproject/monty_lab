40db9b0d931db4398f51d9d349ca33253fbda19b
Scott Knudstrup <scottknudstrup@gmail.com>
2025-03-14 04:49:43 -0700

    Update fig6.py

--- dmc/scripts/fig6.py
+++ dmc/scripts/fig6.py
@@ -36,7 +36,13 @@ from data_utils import (
     load_object_model,
 )
 from matplotlib.lines import Line2D
-from plot_utils import TBP_COLORS, add_legend, axes3d_set_aspect_equal, violinplot
+from plot_utils import (
+    TBP_COLORS,
+    add_legend,
+    axes3d_clean,
+    axes3d_set_aspect_equal,
+    violinplot,
+)
 
 plt.rcParams["font.size"] = 8
 plt.rcParams["font.family"] = "Arial"
@@ -45,29 +51,53 @@ plt.rcParams["svg.fonttype"] = "none"
 OUT_DIR = DMC_ANALYSIS_DIR / "fig6"
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
-exp_dir = VISUALIZATION_RESULTS_DIR / "fig6_curvature_guided_policy"
-detailed_stats_path = exp_dir / "detailed_run_stats.json"
-detailed_stats_interface = DetailedJSONStatsInterface(detailed_stats_path)
-stats = detailed_stats_interface[0]
-# %%
-sm = stats["SM_0"]
-locations = np.array(
-    [obs["location"] for obs in stats["SM_0"]["processed_observations"]]
-)
+def plot_curvature_guided_policy():
+    exp_dir = VISUALIZATION_RESULTS_DIR / "fig6_curvature_guided_policy"
+    detailed_stats_path = exp_dir / "detailed_run_stats.json"
+    detailed_stats_interface = DetailedJSONStatsInterface(detailed_stats_path)
+    stats = detailed_stats_interface[0]
 
-model = load_object_model("dist_agent_1lm", "mug")
-fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
-ax.scatter(model.x, model.y, model.z, color=model.rgba, alpha=0.1, s=10)
+    locations = [obs["location"] for obs in stats["SM_0"]["processed_observations"]]
+    locations = np.array(locations)[:14]
 
-ax.scatter(
-    locations[:, 0],
-    locations[:, 1],
-    locations[:, 2],
-    color=TBP_COLORS["blue"],
-    alpha=1,
-    s=10,
-    zorder=10,
-)
-plt.show()
+    # %matplotlib qt
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4), subplot_kw={"projection": "3d"})
 
-# %%
+    model = load_object_model("dist_agent_1lm", "mug")
+    ax.scatter(
+        model.x,
+        model.y,
+        model.z,
+        color=model.rgba,
+        alpha=0.25,
+        s=5,
+        edgecolor="none",
+    )
+
+    ax.scatter(
+        locations[:, 0],
+        locations[:, 1],
+        locations[:, 2],
+        color=TBP_COLORS["blue"],
+        alpha=1,
+        s=20,
+        marker="v",
+        zorder=10,
+    )
+    ax.plot(
+        locations[:, 0],
+        locations[:, 1],
+        locations[:, 2],
+        color=TBP_COLORS["blue"],
+        alpha=1,
+        zorder=10,
+        lw=2,
+    )
+    ax.set_proj_type("persp", focal_length=0.5)
+    axes3d_clean(ax, grid_color=(1, 1, 1, 1))
+    axes3d_set_aspect_equal(ax)
+    ax.view_init(elev=52, azim=-37, roll=60)
+    plt.show()
+    fig.savefig(OUT_DIR / "curvature_guided_policy.png", dpi=300, bbox_inches="tight")
+    fig.savefig(OUT_DIR / "curvature_guided_policy.svg", bbox_inches="tight")
+
